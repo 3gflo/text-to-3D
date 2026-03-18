4b1b2f41274ce5e291f8956f49e3dd2548a9f776
Garron Floyd <131111496+3gflo@users.noreply.github.com>
2026-03-18 19:28:08 -0400

    compatible with most recent main branch. changed one import in routes.py to omit "backend" redundant path

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -4,7 +4,7 @@ from flask import Blueprint, request, send_file, jsonify, current_app
 from app.services.generation.threeD_generator import split_orthographic_sheet
 from io import BytesIO
 
-from backend.app.services.generation.prompt_generator import SYSTEM_INSTRUCTION
+from app.services.generation.prompt_generator import SYSTEM_INSTRUCTION
 
 # Define the blueprint
 api_bp = Blueprint('api', __name__)
