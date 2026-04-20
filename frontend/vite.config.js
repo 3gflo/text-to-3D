10c5ec9e2abe71543860f76c846c3934ded0c2e1
Garron Floyd <131111496+3gflo@users.noreply.github.com>
2026-04-20 18:34:56 -0400

    Change to mixed reality, add support for VR headset

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ['umpire-frequency-morbidity.ngrok-free.dev'],
     proxy: {
       // Matches any request starting with /api
       '/api': {
-        target: 'http://127.0.0.1:5055', // Your Flask URL
+        target: 'http://127.0.0.1:5055',
         changeOrigin: true,
         secure: false,
       },
